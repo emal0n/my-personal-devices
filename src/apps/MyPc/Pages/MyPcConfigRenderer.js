f881698a42e7d97b61951c0e776eeb3df2ef0756
emal0n <edmundoneto511@hotmail.com>
2026-01-21 22:32:59 -0300

    feat: enhance personal profile in MyPcConfigRenderer with additional skills and updated biography

--- src/apps/MyPc/Pages/MyPcConfigRenderer.js
+++ src/apps/MyPc/Pages/MyPcConfigRenderer.js
@@ -16,13 +16,17 @@ function MyPcConfigRenderer({ appRef }) {
 						</div>
 						<div className="my-pc-title2-container">
 							<a className="my-pc-title">Edmundo.</a>
-							<a>Systems Development ➧ 21y</a>
+							<a>➧ systems development</a>
+							<a>➧ 21y</a>
+							<a>➧ linux enthusiast</a>
+							<a>➧ software engineer</a>
+							<a>➧ android developer</a>
 						</div>
 					</div>
 					<div className="my-pc-info-container-2">
 						<div className="my-pc-title2-container">
 							<a className="my-pc-title-2">Stacks:</a>
-							<a>Loading...:) return soon</a>
+							<a>java, python, node, spring, sql, nextjs, react, angular, typescript, flutter</a>
 						</div>
 					</div>
 				</div>
