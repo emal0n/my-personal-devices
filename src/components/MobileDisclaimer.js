d6c33add719c57dc68068a425fa276d6c8cb2806
emal0n <edmundoneto511@hotmail.com>
2026-01-21 21:45:05 -0300

    chore: update personal references in Clippy and MobileDisclaimer components

--- src/components/MobileDisclaimer.js
+++ src/components/MobileDisclaimer.js
@@ -5,10 +5,10 @@ function MobileDisclaimer() {
 		<div className="mobile-disclaimer">
 			<span>Sorry. This page wasn't built to run on mobile devices yet. Please access it from a computer.</span>
 			<div className="mobile-disclaimer-icons" style={{ display: "flex", justifyContent: "center", columnGap: "10px" }}>
-				<a href="https://github.com/stefani16bit/my-personal-computer">
+				<a href="https://github.com/emal0n/my-personal-devices">
 					<img src="icons/github.svg" style={{ width: "30px", height: "30px" }}></img>
 				</a>
-				<a href="https://x.com/ste_16bit">
+				<a href="https://x.com/emal0nn">
 					<img src="icons/twitter.svg" style={{ width: "30px", height: "30px" }}></img>
 				</a>
 			</div>
