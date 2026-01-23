9b3e52636b01bd7d03006919bd0ba10d95fc2850
emal0n <edmundoneto511@hotmail.com>
2026-01-23 16:09:25 -0300

    feat: enhance MyPcExperiencesRenderer with updated layout and add Rekomendo logo

--- src/apps/MyPc/Pages/MyPcExperiencesRenderer.js
+++ src/apps/MyPc/Pages/MyPcExperiencesRenderer.js
@@ -8,24 +8,43 @@ function MyPcExperiencesRenderer({ appRef }) {
 				<a className="my-pc-renderer-title">experiences</a>
 			</div>
 			<div className="my-pc-renderer-container-2">
-				<div className="my-pc-info-father2-container">
-					<div className="my-pc-setup-info-container">
-						<strong>REKOMENDO (REQUEST SERVICE PLATFORM)</strong>
-						<br/>Feira de Santana, BA
-						<br/><strong>Software Engineer</strong> 05/2025 - 12/2025
-						<br/>● Responsible for updating app features and bug fixes for App, Backend and AdminPanel.
-						<br/>● Created automations and webhooks for reducing repetitive activities.
-						<br/>● Continued developing the activities listed below with more leadership responsibilities, ensuring code quality and reusability.
-						<br/><br/>
-						<strong>REKOMENDO (REQUEST SERVICE PLATFORM)</strong>
-						<br/>Feira de Santana, BA
-						<br/><strong>Junior Software Engineer</strong> 12/2024 - 05/2025
-						<br/>● Created Mobile Apps for Android and IOS using Flutter with great practices of ui and ux principles.
-						<br/>● Unit tests, Maintenance and creating new features on the backend produced in NodeJS.
-						<br/>● Maintenance and creating new features on the AdminPanel produced in Angular.
-						<br/>● Created Project Documentations. that reduced troubleshooting time by 40%
-						<br/>● Experience with API Gateways and Payments services.
-						<br/>● Provided services to REKOMENDO using agile methodologies (Scrum, Kanban, Jira).
+				<div className="my-pc-info-father-container">
+					<div className="my-pc-info-container">
+						<div className="my-pc-title3-container">
+							<strong>REKOMENDO (REQUEST SERVICE PLATFORM)</strong>
+							<br/>Feira de Santana, BA
+							<br/><strong>Software Engineer</strong>
+							<br/>05/2025 - 12/2025
+							<br/><br/>
+							● Responsible for updating app features and bug fixes for App, Backend and AdminPanel.
+							<br/>● Created automations and webhooks for reducing repetitive activities.
+							<br/>● Continued developing the activities listed below with more leadership responsibilities, ensuring code quality and reusability.
+						</div>
+						<div className="my-pc-thinkpad-container">
+							<img src="icons/rekomendo.png" style={{ width: "260px", height: "160px" }}></img>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="my-pc-renderer-container-2">
+				<div className="my-pc-info-father-container">
+					<div className="my-pc-info-container">
+						<div className="my-pc-title3-container">
+							<strong>REKOMENDO (REQUEST SERVICE PLATFORM)</strong>
+							<br/>Feira de Santana, BA
+							<br/><strong>Junior Software Engineer</strong>
+							<br/>12/2024 - 05/2025
+							<br/><br/>
+							● Created Mobile Apps for Android and IOS using Flutter with great practices of ui and ux principles.
+							<br/>● Unit tests, Maintenance and creating new features on the backend produced in NodeJS.
+							<br/>● Maintenance and creating new features on the AdminPanel produced in Angular.
+							<br/>● Created Project Documentations. that reduced troubleshooting time by 40%
+							<br/>● Experience with API Gateways and Payments services.
+							<br/>● Provided services to REKOMENDO using agile methodologies (Scrum, Kanban, Jira).
+						</div>
+						<div className="my-pc-thinkpad-container">
+							<img src="icons/rekomendo.png" style={{ width: "260px", height: "160px" }}></img>
+						</div>
 					</div>
 				</div>
 			</div>
